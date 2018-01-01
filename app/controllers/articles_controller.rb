b1e418c81b0014595a9d1eda6da153548cfa63f1
harisdela <delalicharis@gmail.com>
2018-01-01 21:28:28 +0100

    add controller authorization, user can only update/delete his own account

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
 
   before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
@@ -15,7 +17,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
@@ -50,5 +52,12 @@ class ArticlesController < ApplicationController
 
   def set_article
     @article = Article.find(params[:id])
+  end
+
+  def require_same_user
+    if @article.user != current_user
+      flash[:danger] = "This action is prohibited for you, you can only edit or delete your own articles"
+      redirect_to root_path
+    end
   end
 end
